55cd08dcd44a30617bf7a3f5084fdc5a3191220f
段新雨 <17604861053@163.com>
2026-02-16 09:58:22 +0800

    1

--- photo-timeline-uniapp/vite.config.js
+++ photo-timeline-uniapp/vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     uni(),
   ],
   server: {
     host: true,
     port: 3000,
+    hmr: {
+        host: 'localhost',
+        protocol: 'ws',
+    }
   },
 })
